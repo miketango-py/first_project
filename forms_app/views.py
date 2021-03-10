36f7b4c3bbfbc2acf5ea765a3725fb795bedf495
miketango-py <miketango.win10@gmail.com>
2021-03-10 12:42:05 +0100

    L23 -> django.forms_app.forms.add ( "def contact_form (request)" ) AND django.forms_app.views.add ( "def contacts (request)" )

--- forms_app/views.py
+++ forms_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+#form -- import
 
-# Create your views here.
+#for linking to the pages on 'templates' folder
+from django.shortcuts import render#import render
+
+#import the function 'contact_form' from 'forms.py'
+from .forms import contact_form
+
+#functions -- classes
+def contacts(request):#start contacts
+
+	#variables
+	form = ""; context = ""#form and context for contact page
+
+	#code
+	form = contact_form()#assigning the contact message (with all the variables)
+	context = {"form" : form}#assigning the form to the context (a dictionary)
+
+	#the function returns an .html page
+	return render (request, "contact.html", context)#returning the 'contact.html' page and the contact form
+
+#end contacts
+
+#code
