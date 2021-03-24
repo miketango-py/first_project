7b8349dfba2f4219e57a97b671610dad0ebfbcbb
miketango-py <miketango.win10@gmail.com>
2021-03-24 11:58:34 +0100

    dw

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,7 +3,7 @@ from .views import homepage, menu, chisiamo, variabili, index
 app_name = 'prima_app'
 urlpatterns = [
 
-    path('welcome/', homepage, name='home'),
+    path('welcome/', homepage, name='homepage'),
     path('menu/', menu, name='menu'),
     path('chisiamo/', chisiamo, name='chisiamo'),
     path('variabili/', variabili, name='variabili'),
