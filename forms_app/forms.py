53f423d353fb674ceecc08792f6b983b3e2ae078
miketango-py <miketango.win10@gmail.com>
2021-03-24 12:48:23 +0100

    update forms.py

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import Contatto
 
 class FormContatto(forms.ModelForm): 
-    #nome = forms.CharField()
-    #cognome = forms.CharField()
-    #email = forms.EmailField()
-    #contenuto = forms.CharField(widget=forms.Textarea(attrs={"placeholder": "area testuale"}))
     class Meta: 
         model=Contatto
         fields="__all__"
+        widgets = {
+            'name' : forms.TextInput(attrs = {'placeholder':'Fill this field', 'class':'form-control'}),
+            'surname' : forms.TextInput(attrs = {'placeholder':'Fill this field', 'class':'form-control'}),
+            'email' : forms.EmailInput(attrs = {'placeholder':'Fill this field', 'class':'form-control'}),
+            'context' : forms.Textarea(attrs = {'placeholder':'Write', 'class':'form-control'})
+        }
 
