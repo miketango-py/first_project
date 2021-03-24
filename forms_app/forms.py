b7dd66a58d6681546bca3f8f12a7644ccabb6f05
miketango-py <miketango.win10@gmail.com>
2021-03-24 12:56:57 +0100

    update forms.py

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,7 +1,10 @@
+#from -- import
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Contatto
 
-class FormContatto(forms.ModelForm): 
+#functions -- class
+class FormContatto(forms.ModelForm): #start class FormContatto
     class Meta: 
         model=Contatto
         fields="__all__"
@@ -9,6 +12,25 @@ class FormContatto(forms.ModelForm):
             'name' : forms.TextInput(attrs = {'placeholder':'Fill this field', 'class':'form-control'}),
             'surname' : forms.TextInput(attrs = {'placeholder':'Fill this field', 'class':'form-control'}),
             'email' : forms.EmailInput(attrs = {'placeholder':'Fill this field', 'class':'form-control'}),
-            'context' : forms.Textarea(attrs = {'placeholder':'Write', 'class':'form-control'})
+            'context' : forms.Textarea(attrs = {'placeholder':'Write minimum 20 characters', 'class':'form-control'})
         }
+#end class FormContatto
 
+def clean_context(self): #start clean_context; general sintax = clean_field_to_validate
+    
+    #code
+    data = self.cleaned_data["context"]
+    
+    #"parola" is not admitted
+    if "parola" in data:#start if; error 1
+        raise ValidationError ("The context is violating the site norms!")
+    #end if
+
+    #data too short
+    if len(data) < 20:#start if
+        raise ValidationError("The context is too short!")
+    #end if
+
+    return data
+
+#end clean_context
